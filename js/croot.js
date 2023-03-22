45cd2e1337a5277967665c013b185adf8d32a85d
Rolly Maulana Awangga <rolly@awang.ga>
2023-03-22 14:12:24 +0700

    cobaan

--- js/croot.js
+++ js/croot.js
@@ -3,17 +3,20 @@ import View from 'https://petapedia.github.io/openlayers/src/ol/View.js';
 import TileLayer from 'https://petapedia.github.io/openlayers/src/ol/layer/Tile.js';
 import XYZ from 'https://petapedia.github.io/openlayers/src/ol/source/XYZ.js';
 
+tl = new TileLayer({
+    source: new XYZ({
+      url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+    })
+  });
+vw = new View({
+    center: [ -6.7915964,107.2664323],
+    zoom: 7
+  });
+
 new Map({
   target: 'map',
   layers: [
-    new TileLayer({
-      source: new XYZ({
-        url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
-      })
-    })
+    tl
   ],
-  view: new View({
-    center: [ -6.7915964,107.2664323],
-    zoom: 7
-  })
+  view: vw
 });

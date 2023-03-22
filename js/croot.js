b6ea717da5df89055e97b542ae696d3859164ede
Rolly Maulana Awangga <rolly@awang.ga>
2023-03-22 07:19:31 +0700

    ok

--- js/croot.js
+++ js/croot.js
@@ -1,3 +1,9 @@
 import leaflet from 'https://cdn.skypack.dev/leaflet';
 
 var map = leaflet.L.map('map').setView([51.505, -0.09], 13);
+
+
+leaflet.L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);

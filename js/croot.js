e1f411e76c9fb277b3485c761a3f1a8b5d81a813
Rolly Maulana Awangga <rolly@awang.ga>
2023-03-22 20:32:11 +0700

    markicob

--- js/croot.js
+++ js/croot.js
@@ -2,8 +2,8 @@ import {map,overlay} from './config/peta.js';
 import {fromLonLat} from 'https://petapedia.github.io/ol/v7.3.0/proj.js';
 import {toStringHDMS} from 'https://petapedia.github.io/ol/v7.3.0/coordinate.js';
 import {clickpopup} from './template/popup.js';
-import {setInner,textBlur,onClick} from 'https://jscroot.github.io/element/croot.js';
-
+import {setInner,textBlur,onClick, getValue} from 'https://jscroot.github.io/element/croot.js';
+import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 
 function onClosePopupClick() {
   overlay.setPosition(undefined);
@@ -11,12 +11,28 @@ function onClosePopupClick() {
   return false;
 }
 
+function afterSubmitCOG(result){
+  console.log(result);
+}
+
+function onSubmitPopupClick() {
+  let long = getValue('long');
+  let lat = getValue('lat');
+  let volume = getValue('volume');
+  let data = {long,lat,volume};
+  postWithToken("https://foo.bar","Token","dsf9ygf87h98u479y98dj0fs89nfd7",data,afterSubmitCOG);
+  overlay.setPosition(undefined);
+  textBlur('popup-closer');
+  return false;
+}
+
 onClick('popup-closer',onClosePopupClick);
+onClick('cogbutton',onSubmitPopupClick);
 
 map.on('singleclick', function (evt) {
   const coordinate = evt.coordinate;
   let tile = fromLonLat(coordinate)
-  let msg = clickpopup.replace("#LONG#",coordinate[0]).replace("#LAT#",coordinate[1]).replace('#X#',tile[0]).replace('#Y#',tile[1]).replace('#HDMS#',toStringHDMS(coordinate));
+  let msg = clickpopup.replace("/#LONG#/g",coordinate[0]).replace("/#LAT#/g",coordinate[1]).replace('#X#',tile[0]).replace('#Y#',tile[1]).replace('#HDMS#',toStringHDMS(coordinate));
   setInner('popup-content',msg);
   overlay.setPosition(coordinate);
 });
--- js/template/popup.js
+++ js/template/popup.js
@@ -5,7 +5,9 @@ Lat  : #LAT#<br>
 X   : #X#<br>
 Y   : #Y#<br>
 HDMS : #HDMS#<br>
-Volume : <input type="text" id="name" name="name" required>
+Volume : <input type="text" id="volume" name="volume" required>
+<input type="hidden" id="long" name="long" value="#LONG#">
+<input type="hidden" id="lat" name="lat" value="#LAT#">
 <button id="cogbutton" type="button">Kirim</button><br>
 <a href="#">hapus marker ini</a>
 `

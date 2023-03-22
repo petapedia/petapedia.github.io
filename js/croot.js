55963434eb50e55da37e8f7fb76aa28079fb9697
Rolly Maulana Awangga <rolly@awang.ga>
2023-03-22 15:37:20 +0700

    update anu

--- js/croot.js
+++ js/croot.js
@@ -1,7 +1,7 @@
-import Map from 'https://petapedia.github.io/openlayers/src/ol/Map.js';
-import View from 'https://petapedia.github.io/openlayers/src/ol/View.js';
-import TileLayer from 'https://petapedia.github.io/openlayers/src/ol/layer/Tile.js';
-import XYZ from 'https://petapedia.github.io/openlayers/src/ol/source/XYZ.js';
+import Map from 'https://petapedia.github.io/ol/v7.3.0/Map.js';
+import View from 'https://petapedia.github.io/ol/v7.3.0/View.js';
+import TileLayer from 'https://petapedia.github.io/ol/v7.3.0/layer/Tile.js';
+import XYZ from 'https://petapedia.github.io/ol/v7.3.0/source/XYZ.js';
 
 const tl = new TileLayer({
     source: new XYZ({

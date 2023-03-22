b0ad4db965b447cf72a890a4aba6ed5fb6def5ea
Rolly Maulana Awangga <rolly@awang.ga>
2023-03-22 19:26:58 +0700

    perapihan

--- js/template/popup.js
+++ js/template/popup.js
@@ -1,14 +1,14 @@
 export const clickpopup = `
 <b>Input Center of Gravity</b>
-Long : #LONG#
-Lat  : #LAT#
-XY   : #TILE#
-HDMS : #HDMS#
-Volume : <input type="text" id="name" name="name" required>
+Long : #LONG#<br>
+Lat  : #LAT#<br>
+XY   : #TILE#<br>
+HDMS : #HDMS#<br>
+Volume : <input type="text" id="name" name="name" required><br>
 <button class="favorite styled"
         type="button">
     Kirim
-</button>
+</button><br>
 <a href="#">hapus marker ini</a>
 `
 

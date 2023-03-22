25dfc87ca6f4c88a2b8350236a7ad15a1f651dd4
Rolly Maulana Awangga <rolly@awang.ga>
2023-03-23 05:03:26 +0700

    setvalue

--- js/croot.js
+++ js/croot.js
@@ -2,7 +2,7 @@ import {map,overlay} from './config/peta.js';
 import {fromLonLat} from 'https://petapedia.github.io/ol/v7.3.0/proj.js';
 import {toStringHDMS} from 'https://petapedia.github.io/ol/v7.3.0/coordinate.js';
 import {clickpopup} from './template/popup.js';
-import {setInner,textBlur,onClick, getValue} from 'https://jscroot.github.io/element/croot.js';
+import {setInner,textBlur,onClick, getValue,setValue} from 'https://jscroot.github.io/element/croot.js';
 import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 
 function onClosePopupClick() {
@@ -34,5 +34,7 @@ map.on('singleclick', function (evt) {
   let tile = fromLonLat(coordinate);
   let msg = clickpopup.replace("#LONG#",coordinate[0]).replace("#LAT#",coordinate[1]).replace('#X#',tile[0]).replace('#Y#',tile[1]).replace('#HDMS#',toStringHDMS(coordinate));
   setInner('popup-content',msg);
+  setValue('long',coordinate[0]);
+  setValue('lat',coordinate[1]);
   overlay.setPosition(coordinate);
 });

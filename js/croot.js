91a61a08d15c5a9184ca124e29b5338bd804c381
Rolly Maulana Awangga <rolly@awang.ga>
2023-03-22 07:51:21 +0700

    ok

--- js/croot.js
+++ js/croot.js
@@ -10,3 +10,14 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);

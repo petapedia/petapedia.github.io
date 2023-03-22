2afda2da88c97ba05a930e0987cf88c98f999ed8
Rolly Maulana Awangga <rolly@awang.ga>
2023-03-23 04:34:28 +0700

    tambah atribut

--- js/template/popup.js
+++ js/template/popup.js
@@ -1,14 +1,8 @@
 export const clickpopup = `
-<b>Input Center of Gravity</b><br>
 Long : #LONG#<br>
 Lat  : #LAT#<br>
 X   : #X#<br>
 Y   : #Y#<br>
 HDMS : #HDMS#<br>
-Volume : <input type="text" id="volume" name="volume" required>
-<input type="hidden" id="long" name="long" value="#LONG#">
-<input type="hidden" id="lat" name="lat" value="#LAT#">
-<button id="cogbutton" type="button">Kirim</button><br>
-<a href="#">hapus marker ini</a>
 `
 

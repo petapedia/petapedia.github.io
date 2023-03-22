0626b9d82d6e67f0244e0427aca1467dc23a2b52
Rolly Maulana Awangga <rolly@awang.ga>
2023-03-23 05:26:42 +0700

    onClick

--- js/croot.js
+++ js/croot.js
@@ -1,5 +1,6 @@
 import {map} from './config/peta.js';
 import {onClosePopupClick,onSubmitPopupClick,onMapClick} from './controller/popup.js';
+import {onClick} from 'https://jscroot.github.io/element/croot.js';
 
 onClick('popup-closer',onClosePopupClick);
 onClick('cogbutton',onSubmitPopupClick);

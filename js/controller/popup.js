c651a2987d911b59efe06a065d8a6b2347576aa3
Rolly Maulana Awangga <rolly@awang.ga>
2023-03-23 21:18:49 +0700

    idmarker

--- js/controller/popup.js
+++ js/controller/popup.js
@@ -14,6 +14,7 @@ export function onClosePopupClick() {
 
 export function onDeleteMarkerClick() {
     let idmarker = getValue('idmarker');
+    console.log(idmarker);
     popupinfo.setPosition(undefined);
     deleteMarker(idmarker);
 }

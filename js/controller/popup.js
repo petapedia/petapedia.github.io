9b148ccb7b402d69b81b1d0194462a957959a75a
Rolly Maulana Awangga <rolly@awang.ga>
2023-03-23 08:59:55 +0700

    ./

--- js/controller/popup.js
+++ js/controller/popup.js
@@ -2,7 +2,7 @@ import {toLonLat,fromLonLat} from 'https://petapedia.github.io/ol/v7.3.0/proj.js
 import {toStringHDMS} from 'https://petapedia.github.io/ol/v7.3.0/coordinate.js';
 import {overlay,map} from '../config/peta.js';
 import {clickpopup} from '../template/popup.js';
-import {cogMarker} from 'marker.js';
+import {cogMarker} from './marker.js';
 import {setInner,textBlur,onClick, getValue,setValue} from 'https://jscroot.github.io/element/croot.js';
 import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 

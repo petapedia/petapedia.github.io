c168a2b6206b27d052d7bc9dac311e1a1a924594
Rolly Maulana Awangga <rolly@awang.ga>
2023-03-30 11:08:17 +0700

    sip

--- js/controller/cog.js
+++ js/controller/cog.js
@@ -4,6 +4,9 @@ export function getAllCoordinates(){
     let i=0;
     let sudahhapus=0;
     let pointlist = [];
+    let totaldemand=0;
+    let Xcog=0;
+    let Ycog=0;
     map.getLayers().forEach(layer => {
       if (i !== 0 && sudahhapus === 0) {
         layer.getSource().getFeatures().forEach( feature =>
@@ -15,10 +18,17 @@ export function getAllCoordinates(){
                 xy : feature.get('geometry').flatCoordinates,
             }
             pointlist.push(node);
+            totaldemand=totaldemand+Number(feature.get('volume'));
+            Xcog=Xcog+feature.get('geometry').flatCoordinates[0]*Number(feature.get('volume'));
+            Ycog=Ycog+feature.get('geometry').flatCoordinates[1]*Number(feature.get('volume'));
           }
         );
       }
       i++;
     });
     console.log(pointlist);
+    x=Xcog/totaldemand;
+    y=Ycog/totaldemand;
+    console.log(x);
+    console.log(y);
 }
